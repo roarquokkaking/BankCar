574d594a12e0021afc67e7407fcd19275203d7d5
JungYeon22 <jyp7558@gmail.com>
2024-05-20 14:48:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# BankCar
+
+---
+
+
+### 필수 다운로드 npm 
+```text
+npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
+
+yarn add react-swipeable-views    
+
+npm install react-icons --save
+
+# 아이콘 참고
+https://react-icons.github.io/react-icons/icons/
+```
---
+++ src/main/java/com/example/bankcar/BankCarApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankCarApplication.class, args);
+    }
+
+}
