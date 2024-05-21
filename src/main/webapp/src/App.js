5f9434291e11fe3bf4d76b01c994a6d803b37517
JungYeon22 <jyp7558@gmail.com>
2024-05-21 09:57:11 +0900

    fix : swipeable 버전 오류 수정

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,16 +1,9 @@
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import Header from './components/Header';
-import OptionsTab from './components/OptionsTab';
-import Container from '@mui/material/Container';
-import LocationCards from './components/LocationCards';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import Footer from './components/Footer';
 import FooterMenu from './components/FooterMenu';
-import { displayOnDesktop } from './themes/commonStyles';
-import MobileFooter from './components/MobileFooter';
 import Home from './components/Home';
 import Login from './components/Login';
 import WishList from './components/WishList';
