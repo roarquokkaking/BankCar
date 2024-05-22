29d8b10263740f96d5df3665b9623f4a7adf0fa1
roarquokkaking <ldw9581@naver.com>
2024-05-22 18:02:58 +0900

    feat : 구글 로그인 DB insert

--- src/main/java/com/example/bankcar/BankCarApplication.java
+++ src/main/java/com/example/bankcar/BankCarApplication.java
@@ -2,14 +2,14 @@ package com.example.bankcar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"login.*"})
-//@EntityScan("user.bean")
-//@EnableJpaRepositories("user.dao")
+@EntityScan("login.dto")
+@EnableJpaRepositories("login.dao")
 public class BankCarApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/login/service/LoginService.java
@@ -0,0 +1,10 @@
+package login.service;
+
+import jakarta.servlet.http.HttpSession;
+import login.dto.LoginDTO;
+
+public interface LoginService {
+    void googleLogin(String code, HttpSession session);
+    String isExistId(String id);
+    void insertUser(LoginDTO loginDTO);
+}
