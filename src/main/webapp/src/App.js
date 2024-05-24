9448947e41f39e64eba20f88e7e3a0a5ea907abc
JungYeon22 <jyp7558@gmail.com>
2024-05-24 18:13:47 +0900

    feat : Profile page, header component

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -10,6 +10,14 @@ import Search from "./components/Search";
 import Login_main from './components/login/Login_main';
 import GoogleLogin from './components/login/GoogleLogin';
 import Choice from './components/Choice';
+import ProfileMain from './components/profile/ProfileMain';
+import ReservedCars from './components/profile/ReservedCars';
+import UsedCarReviews from './components/profile/UsedCarReviews';
+import CheckMyCar from './components/profile/CheckMyCar';
+import RegisterMain from './components/register/RegisterMain';
+import {Provider} from 'react-redux';
+import store from './store/store';
+import Payment_main from './components/payment/Payment_main';
 
 function App() {
   return (
@@ -37,9 +45,10 @@ function App() {
               <Route path="usedCarReviews" element={<UsedCarReviews />} />
               <Route path="checkMyCar" element={<CheckMyCar />} />
             </Route>
+            <Route path='/car/new' element={<RegisterMain />} />
             <Route path="/search" element={<Search />} />
             <Route path="/choice" element={<Choice />} />
-
+            <Route path='/payment' element={<Payment_main />} />
           </Routes>
           <Box sx={{ display: { xs: "flex", md: "none" }, marginTop: "auto" }}>
             <FooterMenu />
@@ -49,6 +58,7 @@ function App() {
             <Footer />
           </Box> */}
         </Box>
+
       </BrowserRouter>
     </React.Fragment>
   );
