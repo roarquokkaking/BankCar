a0f3ee1b3272ee8cd3421f35fbcbac8fb0d5b7f0
JungYeon22 <jyp7558@gmail.com>
2024-05-24 18:37:09 +0900

    feat : car image, logout add

--- src/main/webapp/src/components/profile/ProfileMain.jsx
+++ src/main/webapp/src/components/profile/ProfileMain.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import "./ProfilePage.css";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import { Link } from 'react-router-dom';
 import Button from "@mui/material/Button";
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
