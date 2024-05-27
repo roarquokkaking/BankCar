849d53b96fb630dea695db56e30c02c2c0b992d7
kimyonghwan1996 <137673370+kimyonghwan1996@users.noreply.github.com>
2024-05-27 18:59:50 +0900

    추가 : choice 페이지 리뷰 수정

--- src/main/java/search/controller/SearchController.java
+++ src/main/java/search/controller/SearchController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -21,35 +24,13 @@ public class SearchController {
     @Autowired
     SearchService searchService;
 
-    @GetMapping(path = "detail", produces = "application/json; charset=UTF-8")
-    public ResponseEntity<String> naverSearchList(@RequestParam(value="text") String text) {
-        //네이버 검색 클라이언트 ID
-        String clientId = "eERtooMMW67jj2cV5EyJ"; 		
-        String clientSecret = "6MV3Afx8ts";
-
-        String uri = UriComponentsBuilder
-            .fromUriString("https://openapi.naver.com")
-            .path("/v1/search/local.json")
-            .queryParam("query", text)
-            .queryParam("display", 5)
-            .queryParam("start", 1)
-            .queryParam("sort", "random")
-            .build(true)
-            .toUriString();
-
-        WebClient webClient = WebClient.builder()
-            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .defaultHeader("X-Naver-Client-Id", clientId)
-            .defaultHeader("X-Naver-Client-Secret", clientSecret)
-            .build();
-
-        ResponseEntity<String> responseEntity = webClient.get()
-            .uri(uri)
-            .retrieve()
-            .toEntity(String.class)
-            .block();
+    @GetMapping(value = "searched")
+    public String searched(@RequestParam(value = "jibunAddress") String jibunAddress,
+                           @RequestParam(value = "startDate") String startDate,
+                           @RequestParam(value = "endDate") String endDate,
+                           @RequestParam(value = "endTime") String endTime,
+                           @RequestParam(value = "startTime") String startTime, Model model) {
         
-        String responseBody = responseEntity.getBody();
-        return ResponseEntity.ok(responseBody);
+        return "/";
     }
 }
