221453bf018db0dd7e704e51b1bb9068b8fe97c5
JungYeon22 <jyp7558@gmail.com>
2024-06-04 10:57:45 +0900

    fix: 로그인 에러

--- src/main/java/login/dto/LoginDTO.java
+++ src/main/java/login/dto/LoginDTO.java
@@ -10,7 +10,6 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-
 public class LoginDTO {
     @Id
     @Column(name="id")
@@ -28,41 +27,12 @@ public class LoginDTO {
     @Column(name = " create_date",nullable = true)
     private LocalDateTime create_date ;// 가입 날짜
     @Column(name = "rating",nullable = true)
-    private float rating ; //매너 온도
+    private Float rating ; //매너 온도
     @Column(name ="image_file_name",nullable = true)
     private String image_file_name; // 클라우드 이미지 uuid
     @Column(name ="image_original_name",nullable = true)
     private String image_original_name ; //이미지 실제 이름
 
 
-    public boolean isDriver() {
-        return driver;
-    }
-
-    public void setDriver(boolean driver) {
-        this.driver = driver;
-    }
-
-    public String getId() {return id;}
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
