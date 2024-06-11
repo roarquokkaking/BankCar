e6de58fdca52ec861c7dad84ed7527081d82bb2e
roarquokkaking <ldw9581@naver.com>
2024-06-11 10:30:19 +0900

    feat : ssl 인증 및 배포

--- src/main/java/com/example/bankcar/BankCarApplication.java
+++ src/main/java/com/example/bankcar/BankCarApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"login.*","driverLicense.*","chat.*","car.*","search.*"})
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @Import(WebMvcConfig.class)
 @EntityScan(basePackages = {"login.dto","driverLicense.entity","chat.entity","car.entity","search.bean"})
 @EnableJpaRepositories(basePackages = {"login.dao","driverLicense.repo","chat.repository","car.repo","search.dao"})
