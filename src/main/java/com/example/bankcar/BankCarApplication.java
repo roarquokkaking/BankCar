9131e61115b201325e8a70ca8aed8d92b180a734
roarquokkaking <ldw9581@naver.com>
2024-06-13 16:16:28 +0900

    merge

--- src/main/java/com/example/bankcar/BankCarApplication.java
+++ src/main/java/com/example/bankcar/BankCarApplication.java
@@ -10,14 +10,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"login.*","driverLicense.*","chat.*","car.*","search.*","payment.*"})
 @CrossOrigin(origins = "*")
-
-@EntityScan(basePackages = {"login.dto","driverLicense.entity","chat.entity","car.entity","search.bean","payment.entity"})
-@EnableJpaRepositories(basePackages = {"login.dao","driverLicense.repo","chat.repository","car.repo","search.dao","payment.repo"})
 @ComponentScan(basePackages = {"login.*","driverLicense.*","user.*" ,"booking.*","chat.*","car.*","wishList.*","search.*","main.*","payment.*"})
-@EntityScan(basePackages = {"login.dto","driverLicense.entity","chat.dto","booking.entity","chat.bean","car.entity","wishList.entity","chat.entity","search.bean","chat.entity"})
-@EnableJpaRepositories(basePackages = {"login.dao","driverLicense.repo","user.repository","booking.repository", "chat.repository","car.repo","wishList.repository","search.dao","main.repository"})
+@EntityScan(basePackages = {"login.dto","driverLicense.entity","chat.dto","booking.entity","chat.bean","car.entity","wishList.entity","chat.entity","search.bean","chat.entity","payment.entity"})
+@EnableJpaRepositories(basePackages = {"login.dao","driverLicense.repo","user.repository","booking.repository", "chat.repository","car.repo","wishList.repository","search.dao","main.repository","payment.repo"})
 public class BankCarApplication {
 
     public static void main(String[] args) {
