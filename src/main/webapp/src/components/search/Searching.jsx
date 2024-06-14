c9c3abbf62a4fc010b6f9557d67cb9236da7bf52
kimyonghwan1996 <137673370+kimyonghwan1996@users.noreply.github.com>
2024-06-14 11:11:38 +0900

    용환커밋

---
+++ src/main/webapp/src/components/search/Searching.jsx
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate} from 'react-router-dom';
+import axios from 'axios';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import '../../CSS/SearchCSS.css';
+import KakaoMap from './KakaoMap';
+import DateTimePicker from './DateTimePicker';
+import PriceSelect from './PriceSelect';
+
+const Searching = () => {
+    // 달력
+    const [startDate,setStartDate] = useState(null);
+    const [endDate,setEndDate] = useState(null);
+
+    const [startTime, setStartTime] = useState(null);
+    const [endTime, setEndTime] = useState(null);
+
+    const [reset, setReset] = useState(false);
+    const [dateRangeDiv, setDateRangeDiv] = useState('');
+    const [startTimeDiv, setStartTimeDiv] = useState('');
+    const [endTimeDiv, setEndTimeDiv] = useState('');
+
+    const navigate = useNavigate();
+
+    const [searchDTO, setSearchDTO] = useState({
+        startDate:'',
+        endDate:'',
+        startTime:'',
+        endTime:'',
+        jibunAddress: '',
+        roadAddress: '',
+        x: '',
+        y: '',
+        minPrice:'',
+        maxPrice:''
+    })
+
+    const onReset = (e)=>{
+        e.preventDefault()
+        setReset(true);
+        setSearchDTO({
+            startDate: '',
+            endDate: '',
+            startTime: '',
+            endTime: '',
+            jibunAddress: '',
+            roadAddress: '',
+            x: '',
+            y: '',
+            minPrice:'',
+            maxPrice:''
+        });
+        setValidationMessages({
+            dateRange: '',
+            startTime: '',
+            endTime: ''
+        });
+    }
+
+    const [validationMessages, setValidationMessages] = useState({
+        date: '',
+        starttime: '',
+        endtime:''
+    });
+
+    const onSearch = () => {
+        let isValid = true;
+        let newValidationMessages = { date: '', starttime: '', endtime: '' };
+
+        if (searchDTO.startDate === '' || searchDTO.endDate === '') {
+            newValidationMessages.date = '기간을 입력하세요';
+            isValid = false;
+        }
+        else if (searchDTO.startTime === '') {
+            newValidationMessages.starttime = '시간을 입력하세요';
+            isValid = false;
+        }
+        else if (searchDTO.endTime === '') {
+            newValidationMessages.endtime = '시간을 입력하세요';
+            isValid = false;
+        }
+
+        setValidationMessages(newValidationMessages);
+
+        if (isValid) {
+            const params = new URLSearchParams(searchDTO).toString();
+            navigate(`/?${params}`);
+        }
+    };
+
+    return (
+        <div>
+            <div className="search-header"></div>
+            <div className="search-container">
+                <h2>오데가노?</h2>
+                <KakaoMap searchDTO={searchDTO} setSearchDTO={setSearchDTO} />
+                <br/>
+                <DateTimePicker searchDTO={searchDTO} setSearchDTO={setSearchDTO} validationMessages={validationMessages} reset={reset} setReset={setReset}/>
+                <br/>
+                <PriceSelect searchDTO={searchDTO} setSearchDTO={setSearchDTO} fixedMinPrice={10000} fixedMaxPrice={1000000} priceGap={100}/>
+            </div>
+            <div className="search-footer">
+                <div className="clear-button" onClick={onReset}>Clear all</div>
+                <div className="search-button" onClick={onSearch}>Search</div>
+            </div>
+        </div>
+    );
+};
+
+export default Searching;
