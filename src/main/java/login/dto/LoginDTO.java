c4f02779b55f373ed845d4de6dcfd81cf0b2fa46
JungYeon22 <jyp7558@gmail.com>
2024-06-14 11:41:18 +0900

    https://dongwoossltest.shop//api 로 변경

--- src/main/java/login/dto/LoginDTO.java
+++ src/main/java/login/dto/LoginDTO.java
@@ -1,9 +1,11 @@
 package login.dto;
 
+import booking.entity.BookingEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name="user")
