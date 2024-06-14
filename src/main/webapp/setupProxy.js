6df41691ad3794716a6c3327b5f037c4c8207bf8
dnwlwlq123 <dnwlwlq123@naver.com>
2024-06-14 15:03:25 +0900

    feat: 채팅 ui update

--- src/main/webapp/setupProxy.js
+++ src/main/webapp/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
     app.use(
-        "/api/ws",  // 프록시할 경로
+        "/api/chattingroom",  // 프록시할 경로
         createProxyMiddleware({
-            target: "wss://dongwoossltest.shop",  // WebSocket 서버 주소
+            target: "https://dongwoossltest.shop",  // WebSocket 서버 주소
             ws: true,  
             secure: true,  
             changeOrigin: true,  
