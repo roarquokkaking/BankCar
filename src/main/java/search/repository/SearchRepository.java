01e2a7787755c5ab8dcc68b1a9ec632ab80f53f7
kimyonghwan1996 <137673370+kimyonghwan1996@users.noreply.github.com>
2024-06-14 12:31:50 +0900

    용환커밋2

--- src/main/java/search/repository/SearchRepository.java
+++ src/main/java/search/repository/SearchRepository.java
@@ -1,22 +1,22 @@
-package search.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-import search.bean.SearchDTO;
-
-import java.util.*;
-import java.sql.Time;
-@Repository
-public interface SearchRepository extends JpaRepository<SearchDTO, Long>{
-
-       @Query("SELECT cs.car FROM CarServiceEntity cs " +
-       "WHERE cs.startDate <= :endDate AND cs.endDate >= :startDate " +
-       "AND cs.startTime <= :endTime AND cs.endTime >= :startTime " +
-       "AND cs.car.price >= :minPrice AND cs.car.price <= :maxPrice")
-    Optional<SearchDTO> findCarByServiceDatesAndTimesAndPriceRange(
-            Date startDate, Date endDate, Time startTime, Time endTime,
-            Integer minPrice, Integer maxPrice);
-}
+//package search.repository;
+//
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+//import org.springframework.stereotype.Repository;
+//import search.bean.SearchDTO;
+//
+//import java.util.*;
+//import java.sql.Time;
+//@Repository
+//public interface SearchRepository extends JpaRepository<SearchDTO, Long>{
+//
+//       @Query("SELECT cs.car FROM CarServiceEntity cs " +
+//       "WHERE cs.startDate <= :endDate AND cs.endDate >= :startDate " +
+//       "AND cs.startTime <= :endTime AND cs.endTime >= :startTime " +
+//       "AND cs.car.price >= :minPrice AND cs.car.price <= :maxPrice")
+//    Optional<SearchDTO> findCarByServiceDatesAndTimesAndPriceRange(
+//            Date startDate, Date endDate, Time startTime, Time endTime,
+//            Integer minPrice, Integer maxPrice);
+//}
