affd62e7828e91a5bd30ccd1a51be333272d02cb
roarquokkaking <ldw9581@naver.com>
2024-06-20 12:20:20 +0900

    fix : 결제화면 개발중

--- src/main/webapp/src/components/payment/KaKaoPaySuccess.jsx
+++ src/main/webapp/src/components/payment/KaKaoPaySuccess.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import './css/payment.module.css';
+import styles from './css/payment.module.css';
 
 const KaKaoPaySuccess = () => {
     const location=useLocation();
@@ -35,42 +35,49 @@ const KaKaoPaySuccess = () => {
 
 
     return (
-        <div className="App">
-      <div className="confirmation-container">
-        <div className="icon">
+        <div className={styles["App"]}>
+      <div className={styles["confirmation-container"]}>
+        <div className={styles["icon"]}>
           <img src={`${process.env.PUBLIC_URL}/image/paySuccess.png`} alt="Success Icon" />
         </div>
+        <br></br>
         <h1>결제가 완료되었습니다!</h1>
-        <p>주문이 성공적으로 완료되었습니다. .</p>
+        <p>대여가 성공적으로 완료되었습니다. .</p>
         
-        <div className="order-details">
-          <div className="detail-item">
-            <span className="detail-title">예약 차량:</span>
-            <span className="detail-content">{payDetail.item_name}</span>
+        <div className={styles["order-details"]}>
+          <div className={styles["detail-item"]}>
+            <span className={styles["detail-title"]}>예약 차량:</span>
+            <span className={styles["detail-content"]}>{payDetail.item_name}</span>
           </div>
-          <div className="detail-item">
-            <span className="detail-title">결제 금액:</span>
-            <span className="detail-content">{payDetail.total_amount}</span>
+          <br></br>
+          <div className={styles["detail-item"]}>
+            <span className={styles["detail-title"]}>결제 금액:</span>
+            <span className={styles["detail-content"]}>{payDetail.total_amount}</span>
           </div>
-          <div className="detail-item">
-            <span className="detail-title">결제 방법:</span>
-            <span className="detail-content">카카오 페이</span>
+          <br></br>
+          <div className={styles["detail-item"]}>
+            <span className={styles["detail-title"]}>결제 방법:</span>
+            <span className={styles["detail-content"]}>카카오 페이</span>
           </div>
-          <div className="detail-item">
-            <span className="detail-title">대여 주소:</span>
-            <span className="detail-content"></span>
+          <br></br>
+          <div className={styles["detail-item"]}>
+            <span className={styles["detail-title"]}>대여 주소:</span>
+            <span className={styles["detail-content"]}></span>
           </div>
-          <div className="detail-item">
-            <span className="detail-title">대여 시작일:</span>
-            <span className="detail-content"></span>
+          <br></br>
+          <div className={styles["detail-item"]}>
+            <span className={styles["detail-title"]}>대여 시작일:</span>
+            <span className={styles["detail-content"]}></span>
           </div>
-          <div className="detail-item">
-            <span className="detail-title">대여 반납일:</span>
-            <span className="detail-content"></span>
+          <br></br>
+          <div className={styles["detail-item"]}>
+            <span className={styles["detail-title"]}>대여 반납일:</span>
+            <span className={styles["detail-content"]}></span>
           </div>
+          <br></br>
         </div>
-
-        <button className="home-button" onClick={() => window.location.href = '/'}>
+        <br></br>
+        <button className={styles["home-button"]} onClick={() => window.location.href = '/'}>
           홈
         </button>
       </div>
