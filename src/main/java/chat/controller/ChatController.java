72ae1cd3e6e93ae242e475e5421a9cb690ae8c03
dnwlwlq123 <dnwlwlq123@naver.com>
2024-06-20 16:47:28 +0900

    feat: gk.......

--- src/main/java/chat/controller/ChatController.java
+++ src/main/java/chat/controller/ChatController.java
@@ -9,6 +9,7 @@ import login.dto.LoginDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -49,7 +50,38 @@ public class ChatController {
         return userInfo;
     }
 
-    @PostMapping("/send")
+//    @PostMapping("/send")
+//    public ResponseEntity<Message> sendMessage(@RequestBody Message message,
+//                                               @SessionAttribute(name = "loginDTO", required = false) LoginDTO loginDTO) {
+//        try {
+//            if (loginDTO != null) {
+//                String senderName = loginDTO.getName();
+//                message.setSender(senderName);
+//                message.setSentTime(LocalDateTime.now());
+//
+//                // 메시지 RoomSeq 값 검증
+//                if (message.getMessageRoom() == null || message.getMessageRoom().getRoomSeq() == null) {
+//                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null); // BAD_REQUEST(400) 응답 반환
+//                }
+//
+//                // 메시지 저장
+//                Message savedMessage = messageService.saveMessage(message);
+//
+//                Long roomSeq = savedMessage.getMessageRoom().getRoomSeq();
+//                // 메시지 전송
+//                messagingTemplate.convertAndSend("/topic/public" + roomSeq, savedMessage);
+//
+//                return ResponseEntity.ok(savedMessage);
+//            } else {
+//                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // UNAUTHORIZED(401) 응답 반환
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build(); // INTERNAL_SERVER_ERROR(500) 응답 반환
+//        }
+//    }
+
+    @MessageMapping("/send")
     public ResponseEntity<Message> sendMessage(@RequestBody Message message,
                                                @SessionAttribute(name = "loginDTO", required = false) LoginDTO loginDTO) {
         try {
