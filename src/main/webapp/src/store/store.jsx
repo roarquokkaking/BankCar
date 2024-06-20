67ad0e2c31b4ef60ee3ad728d48be86fcb4cbf80
roarquokkaking <ldw9581@naver.com>
2024-06-20 16:04:03 +0900

    fix : 필터 개발중

--- src/main/webapp/src/store/store.jsx
+++ src/main/webapp/src/store/store.jsx
@@ -1,14 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import loginReducer from './loginSlice';
 import optionReducer from './optionSlice';
 
 
+// persist 설정
+const persistConfig = {
+    key: 'root',
+    storage,
+  };
+  
+  // 개별 리듀서에 persist를 적용
+  const persistedLoginReducer = persistReducer(persistConfig, loginReducer);
+  const persistedOptionReducer = persistReducer(persistConfig, optionReducer);
+
 const store = configureStore({
     reducer:{
-        Login:loginReducer,
-        Option:optionReducer
+        Login:persistedLoginReducer,
+        Option:persistedOptionReducer
     }
 
 });
-
-export default store;
+const persistor = persistStore(store);
+export {store,persistor};
