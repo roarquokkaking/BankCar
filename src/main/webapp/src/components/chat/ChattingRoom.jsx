e5554e3c111df49e169239504503f6cb8c206419
dnwlwlq123 <dnwlwlq123@naver.com>
2024-06-21 18:45:33 +0900

    feat: 116

--- src/main/webapp/src/components/chat/ChattingRoom.jsx
+++ src/main/webapp/src/components/chat/ChattingRoom.jsx
@@ -73,7 +73,7 @@ const ChattingRoom = () => {
                 });
             }
         };
-    }, [roomSeq]);
+    }, [roomSeq, connectStompClient]);
 
     useEffect(() => {
         scrollToBottom();
