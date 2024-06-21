01a678a238a42edc866dcb84d7baf0a5dfe42d66
dnwlwlq123 <dnwlwlq123@naver.com>
2024-06-21 10:48:22 +0900

    feat: 하;;

--- src/main/java/chat/controller/ChatController.java
+++ src/main/java/chat/controller/ChatController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -82,6 +83,7 @@ public class ChatController {
 //    }
 
     @MessageMapping("/sendMessage")
+    @SendTo("/topic/public/{roomSeq}")
     public void sendMessage(Message message, @SessionAttribute(name = "loginDTO", required = false) LoginDTO loginDTO) {
         try {
             if (loginDTO != null) {
