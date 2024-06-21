f8c215dce54e63eac604294654a841e21c6559a9
dnwlwlq123 <dnwlwlq123@naver.com>
2024-06-21 13:40:53 +0900

    feat: 후...

--- src/main/java/chat/config/WebSocketConfig.java
+++ src/main/java/chat/config/WebSocketConfig.java
@@ -16,7 +16,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/api/ChattingRoom") // WebSocket 엔드포인트 설정
-                .setAllowedOrigins("https://dongwoossltest.shop").withSockJS();
+                .setAllowedOrigins("https://dongwoossltest.shop").withSockJS()
+                .setHeartbeatTime(1000);
     }
 
 //    @Override
@@ -28,6 +29,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setSendTimeLimit(15 * 1000) // 클라이언트가 PONG 메시지를 보내야 할 시간 (밀리초)
+                .setSendBufferSizeLimit(512 * 1024) // 버퍼 크기 제한 (바이트)
+                .setMessageSizeLimit(128 * 1024); // 최대 메시지 크기 (바이트)
     }
 
 
