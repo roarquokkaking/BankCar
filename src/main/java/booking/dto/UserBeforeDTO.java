264f057c809e0acb05eb4239a76c97994065b99d
Choiinhwan11 <inhwan3596@gmail.com>
2024-06-25 17:25:37 +0900

    이미지 서비스

--- src/main/java/booking/dto/UserBeforeDTO.java
+++ src/main/java/booking/dto/UserBeforeDTO.java
@@ -3,6 +3,7 @@ package booking.dto;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 @SuperBuilder
@@ -21,6 +22,8 @@ public class UserBeforeDTO {
     private String doro;
     private String title;
     private String content;
+    private LocalDate startDate;
+    private LocalDate endDate ;
     private LocalTime startTime;
     private LocalTime endTime;
 //    private enum  Bookingstatus;
